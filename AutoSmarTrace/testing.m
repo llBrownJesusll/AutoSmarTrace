a1e8be088ca48546febc2bd079e2eb1530b9f439
Test <test1@Tests-MacBook-Air.local>
2025-03-16 23:08:18 -0700

    testing.m

--- AutoSmarTrace/testing.m
+++ AutoSmarTrace/testing.m
@@ -4,9 +4,13 @@ test = struct
 test = tester(test)
 disp(test.one)
 disp(test.two)
+disp(test.three)
+disp(test.four)
 end
 
 function test = tester(test)
 test.one = magic(1)
 test.two = magic(2)
+test.three = magic(3)
+test.four = magic(4)
 end
